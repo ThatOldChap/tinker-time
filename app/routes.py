b68f9e13cbbd38da535a58dff59489251b26e64d
Michael Chaplin <michaeljchap@gmail.com>
2023-11-18 17:30:41 -0500

    Chart functionality prelim checked

--- app/routes.py
+++ app/routes.py
@@ -28,7 +28,7 @@ class TradeRecord(enum.Enum):
     WIN = "Win"
     LOSS = "Loss"
 
-# Default Values
+# Initializing Parameters
 starting_balance = 100000
 base_risk = 1000
 risk_to_reward_ratio = 2
@@ -41,6 +41,7 @@ max_losses = 1              # Max # of losses starts with 1
 max_wins = 2                # Max # of wins < 2 results in never being able to increase risk with b2b wins
 num_risk_levels = 2
 compare_baseline = False
+risk_mgt_enabled = False
 
 # Debugging
 def get_pp():
@@ -50,26 +51,39 @@ def get_pp():
 #logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 logging.getLogger('matplotlib.font_manager').disabled = True
+pp = get_pp()
 
 
-# Initializations and Tracking Variables
+# Cache Variables
 last_record = None
 last_altered_record = None
-pp = get_pp()
+last_plot = None
+last_compare_baseline = False
 
 
 @app.route('/')
 @app.route('/index')
 def index():
 
-    plot = updatePlot(starting_balance, risk_to_reward_ratio, base_risk, num_trades, win_rate, num_simulations, \
-                      loss_mgt, win_mgt, max_losses, max_wins, num_risk_levels, compare_baseline)
+    global last_plot, last_record, last_altered_record
 
+    baseline = generateTradeRecord(starting_balance, risk_to_reward_ratio, base_risk, num_trades, win_rate, \
+                                    loss_mgt, win_mgt, max_losses, max_wins, num_risk_levels)
+    plot = generateBaselineChart(baseline)
+    
+    # Cache the appropriate variables
+    last_plot = plot
+    last_record = baseline
+    last_altered_record = last_record
+    
     return render_template('layout.html', plot = plot)
 
 
 @app.route('/update_chart', methods=['POST'])
 def update_chart():
+
+    global last_plot, last_compare_baseline, last_record, last_altered_record
+    baseline_has_updated = False    # This can probably be deleted
 
     # Extract the request's form dictionary
     data = request.form.to_dict()
@@ -103,36 +117,168 @@ def update_chart():
                 compare_baseline = False
             elif value == 'true':
                 compare_baseline = True
-            
+        if key == 'baselineHasUpdated':
+            if value == 'false':
+                baseline_has_updated = False
+            elif value == 'true':
+                baseline_has_updated = True
 
-    plot = updatePlot(starting_balance, risk_to_reward_ratio, base_risk, num_trades, win_rate, num_simulations, \
-                      loss_mgt, win_mgt, max_losses, max_wins, num_risk_levels, compare_baseline)
+    # Logic checks
+    risk_mgt_enabled = not (loss_mgt == LossMgt.NONE and win_mgt == WinMgt.NONE)
+    compare_enabled = compare_baseline and not last_compare_baseline
+    compare_disabled = not compare_baseline and last_compare_baseline
+
+    # Reset the compare switch trackers if the switch is not clicked
+    """ if not compare_enabled and not compare_disabled:
+        compare_enabled = False
+        compare_disabled = False """
+
+    if compare_enabled:
+        # Create compare chart with new baseline and using the original baseline as the altered
+        if risk_mgt_enabled:
+            message = 'Compare switch has been enabled with a RiskMgt strategy being used'
+            baseline = alterTradeRecord(last_record, starting_balance, risk_to_reward_ratio, base_risk, \
+                                        LossMgt.NONE, WinMgt.NONE, max_losses, max_wins, num_risk_levels)
+            compared = last_record
+            plot = generateCompareChart(baseline, compared, compare_baseline)
+
+            # Cache the appropriate records
+            last_record = baseline
+            last_altered_record = compared
+
+        # Use the same baseline chart
+        elif not risk_mgt_enabled:
+            message = 'Compare switch has been enabled with no RiskMgt strategy being used'
+            plot = last_plot
+    
+    elif compare_disabled:
+        # Use prev altered record as new baseline data and create new baseline chart with a single series
+        if risk_mgt_enabled:
+            message = 'Compare switch has been disabled with a RiskMgt strategy still being used'
+            baseline = last_altered_record
+            plot = generateBaselineChart(baseline)
+
+            # Cache the appropriate records
+            last_record = baseline
+
+        # No compare chart is active so existing chart should be the same baseline chart
+        elif not risk_mgt_enabled:
+            message = 'Compare switch has been disabled with no RiskMgt strategy being used'
+            plot = last_plot
+    
+    elif compare_baseline:
+        if baseline_has_updated:
+            # If no risk_mgt strategy is used, keep generating new baseline charts with a single series
+            if not risk_mgt_enabled:
+                message = 'Compare Baseline is enabled with no RiskMgt strategy being used'
+                baseline = generateTradeRecord(starting_balance, risk_to_reward_ratio, base_risk, num_trades, win_rate, \
+                                                loss_mgt, win_mgt, max_losses, max_wins, num_risk_levels)
+                plot = generateBaselineChart(baseline)
+
+                # Cache the appropriate records
+                last_record = baseline
+            
+            # Create compare chart with new baseline and new altered data
+            elif risk_mgt_enabled:
+                logging.debug('Compare Baseline is enabled with a RiskMgt strategy being used')
+                baseline = generateTradeRecord(starting_balance, risk_to_reward_ratio, base_risk, num_trades, win_rate, \
+                                                LossMgt.NONE, WinMgt.NONE, max_losses, max_wins, num_risk_levels)
+                compared = alterTradeRecord(baseline, starting_balance, risk_to_reward_ratio, base_risk, loss_mgt, \
+                                            win_mgt, max_losses, max_wins, num_risk_levels)
+                plot = generateCompareChart(baseline, compared, compare_baseline)
+
+                # Cache the appropriate records
+                last_record = baseline
+                last_altered_record = compared
+            
+        elif not baseline_has_updated:
+            # Update compare chart with same baseline, new altered data
+            if risk_mgt_enabled:
+                message = 'Compare Baseline is enabled and a risk parameter has updated, and a RiskMgt strategy is being used'
+                baseline = last_record
+                compared = alterTradeRecord(baseline, starting_balance, risk_to_reward_ratio, base_risk, loss_mgt, \
+                                            win_mgt, max_losses, max_wins, num_risk_levels)
+                plot = generateCompareChart(baseline, compared, compare_baseline)
+
+                # Cache the appropriate records
+                last_record = baseline
+                last_altered_record = compared
+            
+            # Create new baseline chart with same record
+            elif not risk_mgt_enabled:
+                message = 'Compared Baseline is enabled, and RiskMgt strategies have been disabled'
+                baseline = last_record
+                plot = generateBaselineChart(baseline)
+
+                # Cache the appropriate records
+                last_record = baseline
+
+    elif not compare_baseline:
+        # Revert to the baseline and re-apply the adjusted RiskMgt parameters to get the new chart
+        if not baseline_has_updated and (num_simulations == 1):
+            message = 'Compare Baseline is disabled, risk parameter adjusted with num_simulations = 1'  
+            baseline = alterTradeRecord(last_record, starting_balance, risk_to_reward_ratio, base_risk, LossMgt.NONE, \
+                                            WinMgt.NONE, max_losses, max_wins, num_risk_levels)
+            new_baseline = alterTradeRecord(last_record, starting_balance, risk_to_reward_ratio, base_risk, loss_mgt, \
+                                            win_mgt, max_losses, max_wins, num_risk_levels)
+            plot = generateBaselineChart(new_baseline)
+            
+            # Cache the appropriate records
+            last_record = baseline                
+
+        # Create a new baseline chart if any Strategy parameters are updated
+        #elif baseline_has_updated:
+        else:
+            message = 'Compare Baseline is disabled, strategy parameter adjusted'        
+            baseline_list = []
+            for i in range(num_simulations):
+                baseline = generateTradeRecord(starting_balance, risk_to_reward_ratio, base_risk, num_trades, win_rate, \
+                                                loss_mgt, win_mgt, max_losses, max_wins, num_risk_levels)
+                baseline_list.append(baseline)
+
+            plot = generateMultiBaselineChart(baseline_list, compare_baseline)
+
+            # Cache the appropriate records
+            last_record = baseline_list[-1]
+
+    
+    else:
+        message = 'Undetermined case'
+        
+
+    # Cache tracking variables 
+    last_plot = plot
+    last_compare_baseline = compare_baseline
+    logging.debug(message)
+        
+    """  # When the compare_baseline switch is turned off, remove the compared series from the original plot
+    if not compare_baseline and not baseline_has_updated:    
+        data = generateChartData(last_record, compare_baseline)
+        plot = generateBaselineChart(data[0], data[1], True)
+    
+    # If the compare_baseline switch is enabled but no RiskMgt params are set, still update the baseline chart
+    elif compare_baseline and baseline_has_updated and risk_mgt_enabled:
+        plot = updateBaselineChart(starting_balance, risk_to_reward_ratio, base_risk, num_trades, win_rate, \
+                                    num_simulations, loss_mgt, win_mgt, max_losses, max_wins, num_risk_levels)
+
+    # Update the compared chart any time the switch is enabled
+    elif compare_baseline:
+        plot = updateCompareChart(starting_balance, risk_to_reward_ratio, base_risk, num_trades, win_rate, loss_mgt, \
+                                  win_mgt, max_losses, max_wins, num_risk_levels, compare_baseline, baseline_has_updated, \
+                                  risk_mgt_enabled)
+    else:    
+        plot = updateBaselineChart(starting_balance, risk_to_reward_ratio, base_risk, num_trades, win_rate, \
+                                    num_simulations, loss_mgt, win_mgt, max_losses, max_wins, num_risk_levels) """
+        
 
     # Load the chart into the json response to update the image using jQuery
     response = {
-        'message': f'Chart has successfully been updated',
+        'message': message,
         'plot': plot
     }
 
     return jsonify(response)
 
-
-def updatePlot(starting_balance, risk_to_reward_ratio, base_risk, num_trades, win_rate, num_simulations, \
-               loss_mgt, win_mgt, max_losses, max_wins, num_risk_levels, compare_baseline):
-
-    plot = None
-
-    for i in range(num_simulations):
-        record = generateTradeRecord(starting_balance, risk_to_reward_ratio, base_risk, num_trades, win_rate, \
-                                    loss_mgt, win_mgt, max_losses, max_wins, num_risk_levels)
-        data = generateChartData(record, compare_baseline)
-
-        if i == 0:
-            plot = generateBaselineChart(data[0], data[1], True)
-        else:
-            plot = generateBaselineChart(data[0], data[1], False)
-
-    return plot
 
 # TODO: Add a setting for accounting for b/e trades so that the risk is unadjusted
 def generateTradeRecord(starting_balance, risk_to_reward_ratio, base_risk, num_trades, win_rate,
@@ -255,8 +401,10 @@ def generateTradeRecord(starting_balance, risk_to_reward_ratio, base_risk, num_t
         current_risk_level = new_risk_level
         prev_trade_result = current_trade_result
 
-    # Store the last record for any futher alterations
-    last_record = trade_record
+    # Update the cached last_record with the new record
+    """ global last_record
+    last_record = trade_record """
+
     logging.debug(pp.pprint(trade_record))
 
     return trade_record
@@ -286,11 +434,13 @@ def alterTradeRecord(existing_record, starting_balance, risk_to_reward_ratio, ba
     # Extract the first two header rows and remove them before looping through
     altered_record = [existing_record[0]]
     altered_record.append(existing_record[1])
-    header = existing_record.pop(0)         # Remove header
-    first = existing_record.pop(0)          # Remove trade 0
 
     # Loop through the existing trades rather than generate new ones
     for trade in existing_record:
+
+        # Skip the header row and Trade 0
+        if trade[0] == 'Trade #' or trade[0] == 0:
+            continue
 
         # Extract useful data from the existing record
         trade_num = trade[0]
@@ -380,13 +530,10 @@ def alterTradeRecord(existing_record, starting_balance, risk_to_reward_ratio, ba
         current_risk_level = new_risk_level
         prev_trade_result = current_trade_result
     
-    # Store the last record for any futher alterations
-    last_altered_record = altered_record
+    # Update the cached last_altered_record with the lastest altered record
+    """ global last_altered_record
+    last_altered_record = altered_record """
     logging.debug(pp.pprint(altered_record))
-
-    # Add the header rows back into the existing record
-    existing_record.insert(0, first)
-    existing_record.insert(0, header)
 
     return altered_record
 
@@ -398,15 +545,107 @@ def merge_records(main_record, altered_record):
               'Adj Risk (R)', 'Adj P&L (R)', 'Adj P&L ($)', 'Adj Balance ($)']
     merged_record = [header]
 
-    # Remove the header rows from each record
-    main_record.pop(0)
-    altered_record.pop(0)
-
     for r1, r2 in zip(main_record, altered_record):
+        # Skip the header row
+        if r1[0] == 'Trade #':
+            continue
         merged_record.append([r1[0], r1[1], r1[2], r1[3], r1[4], r1[5], '|', \
                               r2[2], r2[3], r2[4], r2[5]])
+        
+    logging.debug(pp.pprint(merged_record))
     
     return merged_record
+
+
+""" def updateCompareChart(starting_balance, risk_to_reward_ratio, base_risk, num_trades, win_rate, loss_mgt, win_mgt, \
+                       max_losses, max_wins, num_risk_levels, compare_baseline, baseline_has_updated, risk_mgt_enabled):
+    
+    global last_record, last_altered_record, last_plot
+
+    # Only update the Compared chart if there is a RiskMgt strategy used
+    if not risk_mgt_enabled:
+        return last_plot
+    
+    # Generate a new baseline if any of the strategy parameters have changed while compare_baseline is enabled
+    if baseline_has_updated:
+        logging.debug('Generating new baseline record')
+        base_loss_mgt = LossMgt.NONE
+        base_win_mgt = WinMgt.NONE
+        baseline = generateTradeRecord(starting_balance, risk_to_reward_ratio, base_risk, num_trades, win_rate, \
+                                    base_loss_mgt, base_win_mgt, max_losses, max_wins, num_risk_levels)
+    else:
+        logging.debug('Using previous record')
+        baseline = last_record
+
+    # Alter the baseline record with the revised parameters to get the series to compare
+    compared = alterTradeRecord(baseline, starting_balance, risk_to_reward_ratio, base_risk, loss_mgt, win_mgt, \
+                     max_losses, max_wins, num_risk_levels)
+        
+    # Get the data for the plot
+    merged = merge_records(baseline, compared)
+    data = generateChartData(merged, compare_baseline)
+
+    # Clear the plot of any past compared series, and re-add the baseline and the new compared series
+    plot = generateCompareChart(data[0], data[1], data[2], True)
+
+    # Update the cached records with the latest copies    
+    last_record = baseline
+    last_altered_record = compared
+
+    return plot
+ """
+
+def generateBaselineChart(baseline_record):
+
+    global last_record
+
+    # Get the data for the plot
+    data = generateChartData(baseline_record, compare_baseline)
+
+    # Clear the plot of any past series and create a new baseline chart
+    plot = plotBaselineChart(data[0], data[1], True)
+
+    # Update the cached records with the latest copy  
+    """  last_record = baseline_record """
+
+    return plot
+
+
+def generateMultiBaselineChart(baseline_record_list, compare_baseline):
+
+    global last_record
+    
+    for record in baseline_record_list:
+        data = generateChartData(record, compare_baseline)
+
+        # Clear the plot of any past series to start with a fresh chart
+        if record == baseline_record_list[0]:
+            plot = plotBaselineChart(data[0], data[1], True)
+        else:
+            plot = plotBaselineChart(data[0], data[1], False)
+
+    # Cached the first record out of the list
+    """ last_record = baseline_record_list[0] """
+
+    return plot
+
+
+def generateCompareChart(baseline_record, compared_record, compare_baseline):
+
+    global last_record, last_altered_record
+
+    # Get the data for the plot
+    merged = merge_records(baseline_record, compared_record)
+    data = generateChartData(merged, compare_baseline)
+
+    # Clear the plot of any past compared series, and re-add the baseline and the new compared series
+    plot = plotCompareChart(data[0], data[1], data[2], True)
+
+    # Update the cached records with the latest copies    
+    """ last_record = baseline_record
+    last_altered_record = compared_record """
+
+    return plot
 
 
 def generateChartData(trade_record, compare_baseline):
@@ -415,10 +654,12 @@ def generateChartData(trade_record, compare_baseline):
     y1_values = []
     y2_values = []
 
-    # Remove the header row before processing
-    trade_record.pop(0)
-
     for trade in trade_record:
+
+        # Skip Header row
+        if trade[0] == "Trade #":
+            continue
+
         x_values.append(trade[0])
         y1_values.append(trade[5])
         if compare_baseline:
@@ -427,7 +668,7 @@ def generateChartData(trade_record, compare_baseline):
     return [x_values, y1_values, y2_values]
 
 
-def generateBaselineChart(x_values, y_values, clear_plot):
+def plotBaselineChart(x_values, y_values, clear_plot):
 
     # Clear the chart to start with new data each time
     if clear_plot:
@@ -446,10 +687,26 @@ def generateBaselineChart(x_values, y_values, clear_plot):
     return plot
 
 
-def generateCompareChart(x_values, y_values, clear_plot):
-    pass
+def plotCompareChart(x_values, y1_values, y2_values, clear_plot):
 
+    # Clear the chart to start with new data each time
+    if clear_plot:
+        plt.clf()
 
+    x_points = np.array(x_values)
+    y1_points = np.array(y1_values)
+    y2_points = np.array(y2_values)
+
+    plt.plot(x_points, y1_points)
+    plt.plot(x_points, y2_points)
+    plt.title('Equity Curve - Baseline vs. Risk Management')
+    plt.xlabel('# of Trades')
+    plt.ylabel('Account Balance')
+
+    plot = get_plot_img(plt)
+
+    return plot
+    
 
 def get_plot_img(plt):
     img = io.BytesIO()
